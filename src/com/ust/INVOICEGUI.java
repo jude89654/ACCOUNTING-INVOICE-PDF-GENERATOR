76cb6c7a3387461c8817e5231a258a06e27c5f74
jude89654 <jude89654@gmail.com>
2016-05-16 03:11:30 +0800

    added date :D

--- src/com/ust/INVOICEGUI.java
+++ src/com/ust/INVOICEGUI.java
@@ -1,5 +1,6 @@
 package com.ust;
 
+import com.toedter.calendar.JDateChooser;
 import com.ust.model.BeanFactory;
 import com.ust.model.ItemBean;
 import com.ust.model.RecieptBean;
@@ -15,8 +16,9 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.text.NumberFormat;import java.text.DecimalFormat;
-import java.util.ArrayList;
+import java.text.NumberFormat;
+import java.text.DecimalFormat;
+import java.util.*;
 import java.util.List;
 
 
@@ -24,6 +26,8 @@ import java.util.List;
  * Created by Jude on 3/6/2016.
  */
 public class INVOICEGUI extends JFrame {
+
+    //para hanggang dalawang decimal lang
     private static NumberFormat formatter = new DecimalFormat("#.00");
     private JPanel InvoiceMainPanel;
     private JTextField customerTextField;
@@ -31,15 +35,19 @@ public class INVOICEGUI extends JFrame {
     private JTextField itemNameTextField;
     private JTextField quantityTextField;
     private JTextField priceTextField;
+
+    //nice joke :D
     private JScrollPane BoomPanes;
     private JButton ADDITEMButton;
     private JButton DELETEITEMButton;
-    private JTable table1;
-    private JLabel QtyLabel;
+
+
+    //private JTable table1;
+    //private JLabel QtyLabel;
     private JLabel priceLabel;
     private JButton GeneratePDFTextField;
     private JLabel statusLabel;
-    private JLabel TotalLabel;
+    //private JLabel TotalLabel;
     private JTextField vatTextField;
     private JLabel vatStatus;
     private JTextField companyNametextField;
@@ -50,8 +58,13 @@ public class INVOICEGUI extends JFrame {
     private JButton chooseLogoButton;
     private JLabel logoPathLabel;
     private JButton removeLogoButton;
+
+
+    private JPanel DateSelectionPanel;
+    private JDateChooser DateChooser;
+    private JButton CURRENTDATEButton;
     List<Object[]> list = new ArrayList<Object[]>();
-    File logo=null;
+    File logo = null;
 
     DefaultTableModel modelo;
 
@@ -75,6 +88,7 @@ public class INVOICEGUI extends JFrame {
         DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
         centerRenderer.setHorizontalAlignment(JLabel.CENTER);//gawing center
         myTable.setDefaultRenderer(String.class, centerRenderer);
+
         //paglagay ng mga renderer para sa table
         myTable.getColumnModel().getColumn(0).setCellRenderer(centerRenderer);
         myTable.getColumnModel().getColumn(1).setCellRenderer(centerRenderer);
@@ -97,6 +111,7 @@ public class INVOICEGUI extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
+                    //checheck kung may laman ang mga textfield
                     if (itemNameTextField.getText().trim() != "" &
                             quantityTextField.getText().trim() != null &
                             priceLabel.getText().trim() != null &
@@ -109,9 +124,10 @@ public class INVOICEGUI extends JFrame {
                         double price = Double.parseDouble(priceTextField.getText().trim());
                         double vatPercent = Double.parseDouble(vatTextField.getText().trim());
 
-                        if (quantity<=0|price<=0|vatPercent<0)throw new Exception();
+                        //pag negative bawal, tunguna negative ang presyo, batuhin pa ang pera sa customer
+                        if (quantity <= 0 | price <= 0 | vatPercent < 0) throw new Exception();
 
-                        //double vat = price * (vatPercent / 100);
+
                         double total = price * quantity;
 
 
@@ -136,15 +152,17 @@ public class INVOICEGUI extends JFrame {
         DELETEITEMButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                try{
-                if (myTable.getSelectedRow() >= 0) {
-                    modelo.removeRow(myTable.getSelectedRow());
-                    totalize(myTable, amountLabel, vatAmountLabel, totalAmountLabel, Double.parseDouble(vatTextField.getText().trim()));
-                    statusLabel.setText("ROW REMOVED");
-                } else {
-                    statusLabel.setText("NO ROWS TO DELETE");
-                }
-            }catch(Exception ex){
+                try {
+
+                    //checheck kung may pinili na rows
+                    if (myTable.getSelectedRow() >= 0) {
+                        modelo.removeRow(myTable.getSelectedRow());
+                        totalize(myTable, amountLabel, vatAmountLabel, totalAmountLabel, Double.parseDouble(vatTextField.getText().trim()));
+                        statusLabel.setText("ROW REMOVED");
+                    } else {
+                        statusLabel.setText("NO ROWS TO DELETE");
+                    }
+                } catch (Exception ex) {
                     vatStatus.setText("INVALID VAT VALUE!");
                 }
             }
@@ -186,39 +204,47 @@ public class INVOICEGUI extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 //check kung empty
-                if (companyNametextField.getText().trim() != ""
+                if (DateChooser.getDate()!=null
+                        &companyNametextField.getText().trim() != ""
                         & customerTextField.getText().trim() != "" &
                         addressTextField.getText().trim() != "" &
                         recieptNumberTextField.getText().trim() != "") {
-                    try{
-                    ArrayList<ItemBean> itemArray = new ArrayList<ItemBean>();
-                    RecieptBean recieptBean = BeanFactory.createBean(companyNametextField.getText()
-                            , customerTextField.getText(),
-                            addressTextField.getText(),
-                            recieptNumberTextField.getText());
+                    try {
+                        //dito ilalagay ang mga laman ng  table
+                        ArrayList<ItemBean> itemArray = new ArrayList<ItemBean>();
 
-                    double amountdue = 0;
-                    double total = 0;
-                    double vat = 0;
-                    for (int x = 0; x < myTable.getRowCount(); x++) {
-                        amountdue += Double.parseDouble("" + myTable.getValueAt(x, 3));
-                        itemArray.add(BeanFactory.createBean("" + myTable.getValueAt(x, 0)
-                                , Double.parseDouble("" + myTable.getValueAt(x, 1))
-                                , Integer.parseInt("" + myTable.getValueAt(x, 2))
-                                , Double.parseDouble("" + myTable.getValueAt(x, 3))));
-                    }
+                        //ito yung mga personal info
+                        RecieptBean recieptBean = BeanFactory.createBean(companyNametextField.getText()
+                                , customerTextField.getText(),
+                                addressTextField.getText(),
+                                recieptNumberTextField.getText());
 
-                    vat = amountdue * (Double.parseDouble(vatTextField.getText().trim()) / 100);
-                    total = vat + amountdue;
+                        double amountdue = 0;
+                        double total = 0;
+                        double vat = 0;
+                        //dito na nilalagay kada row
+                        for (int x = 0; x < myTable.getRowCount(); x++) {
+                            amountdue += Double.parseDouble("" + myTable.getValueAt(x, 3));
+                            itemArray.add(BeanFactory.createBean("" + myTable.getValueAt(x, 0)
+                                    , Double.parseDouble("" + myTable.getValueAt(x, 1))
+                                    , Integer.parseInt("" + myTable.getValueAt(x, 2))
+                                    , Double.parseDouble("" + myTable.getValueAt(x, 3))));
+                        }
 
-                    pdfBean.CreatePDF(itemArray.toArray(new ItemBean[itemArray.size()]),recieptBean,amountdue,vat,total,
-                            Double.parseDouble(vatTextField.getText()),logo);
+                        vat = amountdue * (Double.parseDouble(vatTextField.getText().trim()) / 100);
+                        total = vat + amountdue;
+
+                        //paggawa ng pdf method.
+                        pdfBean.CreatePDF(itemArray.toArray(new ItemBean[itemArray.size()]), recieptBean, amountdue, vat, total,
+                                Double.parseDouble(vatTextField.getText()), logo,DateChooser.getDate());
 
                         statusLabel.setText("PDF GENERATED");
-                    }catch(Exception ae){
+                    } catch (Exception ae) {
                         statusLabel.setText("ERROR IN GENERATING PDF");
                         ae.printStackTrace();
                     }
+                }else{
+                    statusLabel.setText("SOME FIELDS ARE MISSING INPUT");
                 }
             }
 
@@ -228,39 +254,50 @@ public class INVOICEGUI extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 JFileChooser chooser = new JFileChooser();
+                //for specific file extensions lang tinatanggap
                 FileNameExtensionFilter filter = new FileNameExtensionFilter(
                         "JPG & GIF Images", "jpg", "gif");
                 chooser.setFileFilter(filter);
 
 
-                    int returnVal = chooser.showOpenDialog(INVOICEGUI.this);
+                int returnVal = chooser.showOpenDialog(INVOICEGUI.this);
 
-                    if (returnVal == JFileChooser.APPROVE_OPTION) {
-                        logo = chooser.getSelectedFile();
-                        //This is where a real application would open the file.
-                       // log.append("Opening: " + file.getName() + "." + newline);
-                        logoPathLabel.setText("LOGOPATH:"+logo.getAbsolutePath());
-                    } else {
-                        statusLabel.setText("CHOOSE LOGO CANCELLED");
-                        logoPathLabel.setText("LOGO PATH:");
-                    }
+                if (returnVal == JFileChooser.APPROVE_OPTION) {
+                    logo = chooser.getSelectedFile();
+                    //This is where a real application would open the file.
+                    // log.append("Opening: " + file.getName() + "." + newline);
+                    logoPathLabel.setText("LOGOPATH:" + logo.getAbsolutePath());
+                } else {
+                    statusLabel.setText("CHOOSE LOGO CANCELLED");
+                    logoPathLabel.setText("LOGO PATH:");
                 }
+            }
 
 
         });
         removeLogoButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                logo=null;
+                logo = null;
                 logoPathLabel.setText("LOGO PATH:");
                 statusLabel.setText("REMOVED LOGO");
+            }
+        });
+
+
+        //para sa pagkuha ng currentDate
+        CURRENTDATEButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                DateChooser.setDate(new GregorianCalendar().getTime());
+                System.out.println(DateChooser.getDate());
             }
         });
     }
 
     public static void main(String args[]) {
 
-        //For pampaganda lang, I like purple kasi e
+        //For pampaganda lang, I like purple :D
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 System.out.println(info.getName());
@@ -277,7 +314,7 @@ public class INVOICEGUI extends JFrame {
         new INVOICEGUI();
     }
 
-    //update rows
+    //update total vat
     public static void totalize(JTable table, JLabel amountdueLabel, JLabel vatLabel, JLabel totalamountlabel, double vatPercent) throws Exception {
 
         double amountdue = 0;
@@ -287,17 +324,21 @@ public class INVOICEGUI extends JFrame {
             amountdue += Double.parseDouble("" + table.getValueAt(x, 3));
         }
 
-
-        if(vatPercent<0)throw new Exception();
+        //bawal ang negative na vat
+        if (vatPercent < 0) throw new Exception();
         vat = amountdue * (vatPercent / 100);
         total = vat + amountdue;
 
         System.out.println(amountdue + " " + total + " " + vat);
         amountdueLabel.setText(formatter.format(amountdue));
         vatLabel.setText(formatter.format(vat));
-        totalamountlabel.setText(formatter.format(amountdue+vat));
+        totalamountlabel.setText(formatter.format(amountdue + vat));
 
 
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
     }
 }
 
